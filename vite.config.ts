8f76bfff84a336f50749b280cbd4b7453dc11772
thektm <138784494+thektm@users.noreply.github.com>
2026-08-13 01:28:59 +0330

    hi

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5174,
   },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['admin.sedabox.com'],
+  },
+
   build: {
     target: 'es2022',
     sourcemap: false,
